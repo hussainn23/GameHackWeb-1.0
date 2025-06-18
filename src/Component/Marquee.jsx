c3a7c31b214080380902133fc6fe1d0e9f8b73e5
Samman Qaiser <sammansheikh6@gmail.com>
2025-06-18 18:30:42 +0500

    updated

--- src/Component/Marquee.jsx
+++ src/Component/Marquee.jsx
@@ -1,30 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {Button} from '../components/ui/button'
 export const Marquee = () => {
   return (
-    <div className="bg-[#6900E0]  text-[14px] px-2 py-1 flex items-center justify-between overflow-hidden text-white lg:hidden">
+    <div className="  text-[14px] px-2 py-1 flex items-center justify-between overflow-hidden text-white lg:hidden">
       {/* `whitespace-nowrap` prevents line‑breaks */}
-    <Link
+      <Button className='bg-[#6900E0] p-2'>
+         <Link
     to='/earningapps'
     state={{ category: 'COLOUR TRADING' }}
     className='transition duration-300 ease-in-out hover:scale-105 hover:text-[#E3E3E3] hover:underline underline-offset-4'
   >
     COLOR TRADING APPS
   </Link>
-  <Link
+      </Button>
+      <Button className='bg-[#6900E0] p-2'>
+          <Link
     to='/earningapps'
     state={{ category: 'RUMPY GAMES' }}
     className='transition duration-300 ease-in-out hover:scale-105 hover:text-[#E3E3E3] hover:underline underline-offset-4'
   >
     RUMPY APPS
   </Link>
-  <Link
+      </Button>
+<Button className='bg-[#6900E0] p-2'>
+   <Link
     to='/earningapps'
     state={{ category: 'YONO GAMES' }}
     className='transition duration-300 ease-in-out hover:scale-105 hover:text-[#E3E3E3] hover:underline underline-offset-4'
   >
     YONO APPS
   </Link>
+</Button>
+ 
     </div>
   );
 };
