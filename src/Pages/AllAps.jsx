ce3116fb24e7b36ae5a1827d5d917b04f1e95519
Samman Qaiser <sammansheikh6@gmail.com>
2025-06-19 10:37:32 +0500

    updated

--- src/Pages/AllAps.jsx
+++ src/Pages/AllAps.jsx
@@ -167,8 +167,8 @@ useEffect(() => {
 
       {/* RIGHT SECTION - Related Apps */}
       <div className="lg:w-[50%] sm:w-[100%] md:w-[100%]">
-        <h1 className="text-2xl sm:py-2  lg:text-[2rem] font-bold text-[#8900C1] uppercase">Related APPS</h1>
-        <div className="mt-2  grid sm:grid-cols-2  lg:grid-cols-2 lg:gap-4 sm:gap-3 2xl:grid-cols-3">
+        <h1 className="text-2xl sm:py-3  lg:text-[2rem] font-bold text-[#8900C1] uppercase">Related APPS</h1>
+        <div className="mt-1  grid sm:grid-cols-2  lg:grid-cols-2 lg:gap-4 sm:gap-3 2xl:grid-cols-3">
           {tradingCards.map((item, index) => (
             <div  onClick={()=>setID(item.id)}>
                  <TradingCards
